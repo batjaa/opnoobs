3c4d55548de0553228a8687bfadb8ba26ca417fc
batjaa <batjaa_10@yahoo.de>
2015-03-05 05:55:23 -0800

    Configured grunt

--- server/app.js
+++ server/app.js
@@ -18,12 +18,27 @@ app.set('port', process.env.PORT || 3000);
 // Public
 app.use(express.static(projpath('public')));
 
+app.use(function(req, res, next){
+  res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
+  next();
+});
+
 app.get('/', function(req, res){
   res.render('home');
 });
 
 app.get('/about', function(req, res){
-  res.render('about');
+  res.render('about', {
+    pageTestScript: '/qa/tests-about.js'
+  });
+});
+
+app.get('/tours/hood-river', function(req, res){
+  res.render('tours/hood-river');
+});
+
+app.get('/tours/request', function(req, res){
+  res.render('tours/request-group-rate');
 });
 
 // custom 404 page
@@ -35,7 +50,7 @@ app.use(function(req, res){ res.type('text/plain');
 app.use(function(err, req, res, next){
   console.error(err.stack);
   res.status(500);
-  res.render('500')
+  res.render('500');
 });
 
 app.listen(app.get('port'), function(){
